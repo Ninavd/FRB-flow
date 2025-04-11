583fb134678bdec0e5fc18dec54bd23842377074
Nina van der Meulen <ninaajwzz@gmail.com>
2025-04-11 15:13:35 +0200

    2D case working

--- src/helpers.py
+++ src/helpers.py
@@ -178,6 +178,30 @@ def plot_simulated_burst(time, x_counts, x_model=None):
     ax.set_xlim(time[0], time[-1])
     ax.legend()
 
+def plot_trace(sampler, steps, true_value, ylabel="$t_0$"):
+    nwalkers = sampler.nwalkers
+
+    plt.figure(figsize=(12,3))
+
+    # each walker a different color
+    plt.subplot(121)
+    for i, walker in enumerate(sampler.get_chain().reshape((steps, nwalkers)).T):
+        start = int(i * steps)
+        plt.plot(range(start, start + steps), walker)
+
+    plt.title("MCMC traceplot")
+    plt.xlabel("step")
+    plt.ylabel(ylabel)
+    plt.hlines([true_value], xmin=0, xmax=nwalkers*steps, linestyle='dashed', color='black')
+
+    # walkers back to back
+    plt.subplot(122)
+    plt.plot(sampler.get_chain(flat=True), 'k-')
+    plt.title("MCMC traceplot")
+    plt.xlabel("step")
+    plt.ylabel(ylabel)
+    plt.hlines([true_value], xmin=0, xmax=nwalkers*steps, linestyle='dashed', color='red')
+
 
 if __name__=="__main__":
     time = np.linspace(0, 1.0, 1000)
