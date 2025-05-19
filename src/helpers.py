b2b3f6af09eb18ba3879aa3953b139864961cb2f
Nina van der Meulen <ninaajwzz@gmail.com>
2025-05-19 18:54:48 +0200

    split up notebooks

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch 
+import matplotlib.cm as cm
 
 def plot_trace(sampler, steps, true_value, ylabel="$t_0$"):
     nwalkers = sampler.nwalkers
@@ -24,6 +26,46 @@ def plot_trace(sampler, steps, true_value, ylabel="$t_0$"):
     plt.xlabel("step")
     plt.ylabel(ylabel)
     plt.hlines([true_value], xmin=0, xmax=nwalkers*steps, linestyle='dashed', color='red')
+
+# Several plotting utility functions
+def hist2d_samples(samples, bins: int = 200, percentile: int = 99, range=None, **kwargs):
+    """
+    Plots 2d histogram of given (x,y) samples. 
+
+    Parameters:
+      - samples (N, 2): samples to plot
+      - bins (int): Number of bins
+      - percentile (int): Used for color normalization
+      - range (int): range of the histogram bins
+    """
+    if range is None:
+        xmin, xmax = min(samples[:, 0]), max(samples[:, 0])
+        ymin, ymax = min(samples[:, 1]), max(samples[:, 1])
+        range = [[xmin, xmax], [ymin, ymax]]
+
+    H, xedges, yedges = np.histogram2d(samples[:, 0], samples[:, 1], bins=bins, range=range)
+
+    # Determine color normalization based on the 99th percentile
+    cmax = np.percentile(H, percentile)
+    cmin = 0.0
+    norm = cm.colors.Normalize(vmax=cmax, vmin=cmin)
+
+    # Plot using imshow for more control
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    plt.imshow(H.T, origin='lower', extent=extent, norm=norm, **kwargs)
+
+def record_every(num_timesteps: int, record_every: int) -> torch.Tensor:
+    """
+    Compute the time indices corresponding to a `record_every` parameter.
+    """
+    if record_every == 1:
+        return torch.arange(num_timesteps)
+    return torch.cat(
+        [
+            torch.arange(0, num_timesteps - 1, record_every),
+            torch.tensor([num_timesteps - 1]),
+        ]
+    )
 
 
 if __name__=="__main__":
