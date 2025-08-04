5fc8037ab055158c86a66b4f4306ff0bf612a7cf
Nina van der Meulen <ninaajwzz@gmail.com>
2025-08-04 14:20:06 +0200

    docstrings + styling updated

--- src/flow_matching/plotting.py
+++ src/flow_matching/plotting.py
@@ -5,6 +5,7 @@ import os
 
 from corner import corner
 from numpy.lib.stride_tricks import sliding_window_view
+from typing import Iterable, Any 
 
 from src.simulator import Model, BurstSimulator
 from src.flow_matching.integration import EulerODESolver
@@ -17,16 +18,24 @@ import torch
 
 from src.helpers import gen_parameter_labels
 
-# Several plotting utility functions
-def hist2d_samples(samples, bins: int = 200, percentile: int = 99, range=None, **kwargs):
-    """
-    Plots 2d histogram of given (x,y) samples. 
+def hist2d_samples(
+        samples: np.ndarray, 
+        bins: int = 200, 
+        percentile: int = 99, 
+        range:list[float]=None, 
+        **kwargs
+        ):
+    """Plots 2d histogram of given (x,y) samples. 
 
-    Parameters:
-      - samples (N, 2): samples to plot
-      - bins (int): Number of bins
-      - percentile (int): Used for color normalization
-      - range (int): range of the histogram bins
+    Args:
+        samples (ndarray): samples of shape (N, 2).
+        bins (int): Number of bins. Default is 200.
+        percentile (int): Used for color normalization.
+        range (list of float): range of the histogram bins. If None is calculated using min and max of samples.
+        **kwargs: Keyword args to pass to `plt.imshow`
+    
+    Returns:
+        list of int: bin range of the histogram
     """
     if range is None:
         xmin, xmax = min(samples[:, 0]), max(samples[:, 0])
@@ -46,8 +55,26 @@ def hist2d_samples(samples, bins: int = 200, percentile: int = 99, range=None, *
     plt.imshow(H.T, origin='lower', extent=extent, norm=norm, aspect=aspect, **kwargs)
     return range
 
-def plot_loss(losses, window_size=None, ylog=True, xlog=True, save_path=None, show=True, **kwargs):
-    """Plot evolution of loss over time."""
+def plot_loss(
+        losses: np.ndarray[float], 
+        window_size: int|None =None, 
+        ylog: bool=True, 
+        xlog: bool=True, 
+        save_path: str=None, 
+        show: bool=True, 
+        **kwargs
+        ):
+    """Plot evolution of loss over time.
+    
+    Args:
+        losses (array of floats): loss at each step.
+        window_size (int): Used for calculating running average.
+        ylog (bool): set yscale to log. Default is `True`.
+        xlog (bool): set xscale to log. Default is `True`.
+        save_path (str or None): Path to saving directory. Default is None (no saving).
+        show (bool): Show plot with `plt.show`. Default is True. 
+        **kwargs: Keyword arguments passed to `plt.plot`
+    """
 
     # calculate running average
     if window_size:
@@ -71,50 +98,86 @@ def plot_loss(losses, window_size=None, ylog=True, xlog=True, save_path=None, sh
     
     plt.show() if show else None
 
-def plot_snapshots(xts, ts, record_every_idxs, num_marginals, inf_params, N, save_path=None, show=True):
-        """
-        Plot snapshots of the marginal probability path.
+def plot_snapshots(
+        xts: torch.Tensor, 
+        ts: torch.Tensor, 
+        record_every_idxs: torch.Tensor, 
+        num_marginals: int, 
+        inf_params: list[str], 
+        N: int, 
+        save_path: str | None=None, 
+        show: bool=True
+        ) -> torch.Tensor:
+    """
+    Plot snapshots of the marginal probability path.
 
-        Args
-            xts: (B, nts, dim) nts snapshots of each trajectory.
-            ts: 
-            record_every_idxs:
-            num_marginals: number of snapshots to plot.
-            save_path: path to saving directory.
+    Args
+        xts (torch.Tensor): Tensor of shape (B, nts, dim) with snapshots of B trajectories.
+        ts (torch.Tensor): Tensor of shape (nts,) with timestamps corresponding to snapshots.
+        record_every_idxs (torch.Tensor): (num_marginals,) Time indices of snapshots to be plot.
+        num_marginals (int): Number of snapshots to plot.
+        inf_params (list of str): Inference parameter names. 
+        N (int): Number of burst components. 
+        save_path (str or None): Path to saving directory. The plot is not saved if None.
+        show (bool): Show plot with `plt.show`.
 
-        Returns
-            xx: (B, 1, dim) Final snapshot
-        """
-        plt.figure(figsize=(15, 4))
+    Returns
+        torch.Tensor: Final snapshot of shape (B, 1, dim) 
+    """
+    plt.figure(figsize=(15, 4))
 
-        labels = gen_parameter_labels(inf_params, N)
+    labels = gen_parameter_labels(inf_params, N)
 
-        for idx in range(xts.shape[1]):
-            xx = xts[:,idx,:]
-            t = ts.cpu()[record_every_idxs[idx]]
+    for idx in range(xts.shape[1]):
+        xx = xts[:,idx,:]
+        t = ts.cpu()[record_every_idxs[idx]]
 
-            plt.subplot(1, num_marginals, idx + 1)
-            
-            range_ = None if idx == 0 else range_
-            range_ = hist2d_samples(xx.cpu(), range=range_, percentile=100, cmap='Blues')
-            
-            plt.title(f"Learned, t={t:.2f}")
-            plt.xlabel(labels[0])
-            plt.ylabel(labels[1]) if idx == 0 else None
-
-        if save_path:
-            filepath = os.path.join(save_path, 'marginal_path_snapshots.png')
-            plt.savefig(filepath, bbox_inches="tight")
+        plt.subplot(1, num_marginals, idx + 1)
         
-        plt.tight_layout()
-        plt.show() if show else None
+        range_ = None if idx == 0 else range_
+        range_ = hist2d_samples(xx.cpu(), range=range_, percentile=100, cmap='Blues')
+        
+        plt.title(f"Learned, t={t:.2f}")
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1]) if idx == 0 else None
 
-        return xx
+    if save_path:
+        filepath = os.path.join(save_path, 'marginal_path_snapshots.png')
+        plt.savefig(filepath, bbox_inches="tight")
+    
+    plt.tight_layout()
+    plt.show() if show else None
 
-def corner_plot(samples:np.ndarray, burstparams, inf_params, N, save_path: str | None, show: bool):
+    return xx
+
+def corner_plot(
+        samples: np.ndarray, 
+        burstparams: dict[str, Iterable], 
+        inf_params: list[str], 
+        N: int, 
+        save_path: str | None, 
+        show: bool, 
+        **kwargs
+        ):
+    """
+    Plot corner plot from posterior samples.
+
+    Args
+        samples (ndarray): Array with posterior samples of shape (B, dim).
+        ts (torch.Tensor): Tensor of shape (nts,) with timestamps corresponding to snapshots.
+        burstparams (dict[str, Iterable]): Parameters and their true value for each burst component.
+        inf_params (list of str): Inference parameter names. 
+        N (int): Number of burst components. 
+        save_path (str or None): Path to saving directory. If None, the plot is not saved.
+        show (bool): Show plot with `plt.show()`
+        **kwargs: Arguments to pass to corner function.
+
+    Returns
+        torch.Tensor: Final snapshot of shape (B, 1, dim) 
+    """
     var_names = gen_parameter_labels(inf_params, N)
     true_values = np.array([burstparams[key] for key in inf_params]).flatten()
-    fig = corner(samples, labels=var_names, truths=true_values)
+    fig = corner(samples, labels=var_names, truths=true_values, **kwargs)
 
     if save_path:
         filepath = os.path.join(save_path, 'corner_plot.png')
@@ -122,12 +185,43 @@ def corner_plot(samples:np.ndarray, burstparams, inf_params, N, save_path: str |
 
     plt.show() if show else None
 
-def evaluation_plots(losses, vector_field, path, device, 
-                     num_samples, inf_params, N, modelparams, mean, std,
-                     save_path, show_plots, 
-                     loss=True, snapshots=True, make_corner=True, 
-                    ):
-
+def evaluation_plots(
+        losses: np.ndarray[float], 
+        vector_field: torch.nn.Module, 
+        path: torch.nn.Module, 
+        device: torch.device, 
+        num_samples: int, 
+        inf_params: list[str], 
+        N: int, 
+        modelparams: dict[str, Any], 
+        mean: torch.Tensor, 
+        std: torch.Tensor,
+        save_path: str|None, 
+        show_plots: bool, 
+        loss: bool=True, snapshots: bool=True, make_corner: bool=True, 
+        ):
+    """Evaluation plots made after training.
+    
+    Args:
+        losses (array of floats): loss at each step.
+        vector_field (nn.Module): Trained flow matching model. 
+        path (nn.Module): Probability path used in training. 
+        device (torch.device): Evaluate model on this device.
+        num_samples (int): Number of samples used at inference.
+        inf_params (list of str): Inference parameter names. 
+        N (int): Number of burst components.
+        modelparams (dict): Dictionary with kwargs for `Model`, the burst model. 
+            The learned vector field is evaluated on a burst with these parameters.
+        mean (torch.Tensor): Tensor of shape (), estimated mean of the prior. 
+            Used for standardization of prior samples.
+        std (torch.Tensor): Tensor of shape (), estimated standard deviation of the prior. 
+            Used for standardization of prior samples.
+        save_path (str or None), 
+        show_plots (bool): show plots with `plt.show` 
+        loss (bool): Whether to plot the loss. 
+        snapshots (bool): Whether to plot the snapshots. Only works if the vector field is 2D. 
+        make_corner (bool): Whether to plot the corner plot. 
+    """
     # plot loss 
     if loss:
         plot_loss(losses, window_size=10, xlog=False, save_path=save_path, show=False)
